6bfc43a45219e5a8def2bd400e5b76d393d03ad4
Unknown <anatoliy.kuznetsov@gmail.com>
2017-09-02 13:49:05 -0400

    tweaks to build and test system

---
+++ tests/stress/build_all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+#  build_all.sh
+#  
+#
+#  Created by Anatoliy Kuznetsov on 9/2/17.
+#
+
+make clean
+
+rm -rf ./stress_* ./test
+make DEBUG=YES rebuild
+mv ./test ./stress_debug
+
+make rebuild
+mv ./test ./stress_release
+
+make BMOPTFLAGS=-DBMSSE2OPT rebuild
+mv ./test ./stress_release_sse2
+
+make BMOPTFLAGS=-DBMSSE42OPT rebuild
+mv ./test ./stress_release_sse42
+
+make BMOPTFLAGS=-DBM64OPT rebuild
+mv ./test ./stress_release_64
--- tests/stress/run_all.sh
+++ tests/stress/run_all.sh
@@ -7,29 +7,29 @@
 #
 
 echo "DEBUG"
-#./perf_debug || exit 1
+#./stress_debug || exit 1
 
 echo
 echo
 echo RELEASE
 
-./perf_release || exit 1
+./stress_release || exit 1
 
 echo
 echo
 echo SSE2
 
-./perf_release_sse2 || exit 1
+./stress_release_sse2 || exit 1
 
 echo
 echo
 echo SSE42
 
-./perf_release_sse42 || exit 1
+./stress_release_sse42 || exit 1
 
 echo
 echo
 echo 64-bit
 
 
-./perf_release_64 || exit 1
+./stress_release_64 || exit 1
