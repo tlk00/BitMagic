fddc7687d59788507834acfcdf8118a4adb47a76
Unknown <anatoliy.kuznetsov@gmail.com>
2017-11-07 22:10:05 -0500

    changes

--- xcode/bm.build/Debug/ALL_BUILD.build/Script-DCD087F12DF94E4E9579CB94.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/anatoliykuznetsov/Desktop/dev/git/BitMagic/xcode -f /Users/anatoliykuznetsov/Desktop/dev/git/BitMagic/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
