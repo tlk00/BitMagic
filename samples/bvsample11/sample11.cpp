3b343f847041dc932cfe54ab04152f66f00dc62a
tlk00 <anatoliy.kuznetsov@gmail.com>
2017-11-12 15:44:28 -0500

    Added more benchmarks

--- samples/bvsample11/sample11.cpp
+++ samples/bvsample11/sample11.cpp
@@ -29,21 +29,25 @@ For more information please visit:  http://bitmagic.io
 */
 
 /** \example sample11.cpp
-  Demo on memory consumption control techniques
+  Demo and a benchmark on memory consumption control and logical operation
  
- */
+*/
 
 
 #include <iostream>
 #include <memory>
 #include <map>
 #include <vector>
+#include <chrono>
+
 
 #include "bm.h"
-#include "bmrandom.h"
+#include "bmserial.h"
+#include "bmalgo.h"
+#include "bmtimer.h"
 
 // ----------------------------------------------------
-// Global parameters
+// Global parameters and types
 // ----------------------------------------------------
 
 // Number of vectors generated for the test
@@ -55,8 +59,22 @@ const unsigned max_size = 2000000;
 // Number of bits per one vector
 const unsigned bits_per_vect = 5;
 
+// benchmark operation count
+const unsigned benchmark_ops = 1000;
+
+// subset of vectors used as a sample
+const unsigned sample_cnt = 250;
+
+// index values to extract
+const unsigned result_set_cnt = 200;
+
+
 // bit-vector type for this example
 typedef  bm::bvector<>   TBVector;
+
+
+// timing storage for benchmarking
+bm::chrono_taker::duration_map_type  timing_map;
 
 
 
@@ -131,6 +149,17 @@ struct bvs_index
     map_type  idx_;
 };
 
+// Sample index structure to keep map of in-memory vector<unsigned int>
+//
+struct vect_index
+{
+    typedef std::vector<unsigned int>           buffer_type;
+    typedef std::map<unsigned, buffer_type>      map_type;
+    
+    map_type  idx_;
+};
+
+
 // --------------------------------------------------------------------
 
 
@@ -144,7 +173,7 @@ void generate_random_vector(TBVector* bv)
     if (method == 0) // generate a incremental linear sequence at random location
     {
         unsigned seed_id = rand() % max_size;
-        for (unsigned i  = seed_id; i < bits_per_vect; ++i)
+        for (unsigned i = seed_id; i < seed_id+bits_per_vect; ++i)
         {
             if (i >= max_size)
                 break;
@@ -157,7 +186,9 @@ void generate_random_vector(TBVector* bv)
         {
             unsigned id = rand() % max_size;
             if (i >= max_size) // paranoiya check
+            {
                 break;
+            }
             bv->set_bit(id);
         } // for i
     }
@@ -172,6 +203,13 @@ void generate_bv_index(bv_index& bvi)
         std::auto_ptr<TBVector> ap(construct_bvector());
         
         generate_random_vector(ap.get());
+        
+        if (!ap->any()) // integrity check
+        {
+            // this should never happen
+            std::cerr << "Warning. Empty vector generated!" << std::endl;
+        }
+
         bvi.idx_[i] = ap.release();
     }
 }
@@ -186,29 +224,342 @@ size_t calc_memory_footprint(const bv_index& bvi)
          ++it)
     {
         const TBVector* mp = it->second;
+        
         TBVector::statistics st;
         mp->calc_stat(&st);
         mem_total += st.memory_used;
+        
+        mem_total += sizeof(void*);
     } // for
     
     return mem_total;
 }
+
+// convert bit-vector index to bit-vector serialized index
+//
+size_t convert_bv2bvs(const bv_index& bvi, bvs_index& bvs)
+{
+    size_t  mem_total = 0;
+    std::vector<unsigned char> buf;      // prepare a temporary buffer
+    buf.reserve(1024);
+
+    // bit-vector serializer
+    // (keep it out of the serialization loop to minimize buffers re-allocations)
+    //
+    bm::serializer<TBVector> bvsr;
+    bvsr.byte_order_serialization(false);
+    bvsr.gap_length_serialization(false);
+    bvsr.set_compression_level(4);
+    
+    for (bv_index::map_type::const_iterator it = bvi.idx_.begin();
+         it != bvi.idx_.end();
+         ++it)
+    {
+        unsigned id = it->first;
+        const TBVector* bvp = it->second;
+        
+        TBVector::statistics st;
+        bvp->calc_stat(&st);  // calculate max. serialized size
+        
+        buf.resize(st.max_serialize_mem); // prepare the temp buffer
+        
+        // run serialization, actual serialization size is expacted to be smaller
+        //
+        unsigned bvs_size = bvsr.serialize(*bvp, buf.data(), st.max_serialize_mem);
+        
+        // move from temp serialization buffer to compressed in-memory index
+        //
+        bvs_index::buffer_type& vbuf = bvs.idx_[id];
+        vbuf.resize(bvs_size);
+        ::memcpy(vbuf.data(), buf.data(), bvs_size);
+        
+        mem_total += bvs_size;
+        
+        mem_total += sizeof(std::vector<unsigned char>::size_type);
+
+        
+        // paranoia check compare source and desirialized vectors
+        //
+        #ifdef DEBUG
+        {
+            TBVector bv1;
+            bm::deserialize(bv1, vbuf.data());
+            if (bv1.compare(*bvp) !=0 )
+            {
+                std::cerr << "Deserialization check failed!" << std::endl;
+                exit(1);
+            }
+        }
+        #endif
+
+    } // for
+    
+    return mem_total;
+}
+
+// convert bit-vector index to vector<usingned>
+//
+size_t convert_bv2vect(const bv_index& bvi, vect_index& vidx)
+{
+    size_t  mem_total = 0;
+    
+    for (bv_index::map_type::const_iterator it = bvi.idx_.begin();
+         it != bvi.idx_.end();
+         ++it)
+    {
+        unsigned id = it->first;
+        const TBVector* bvp = it->second;
+        
+        unsigned count = bvp->count();  // population count
+        
+        vect_index::buffer_type& vect = vidx.idx_[id];
+        vect.resize(count);
+
+        for (TBVector::enumerator en = bvp->first(); en.valid(); ++en)
+        {
+            vect.push_back(*en);
+        }
+        
+        mem_total +=
+            sizeof(vect_index::buffer_type::value_type) * vect.size() +
+            sizeof(vect_index::buffer_type::size_type);
+
+    } // for
+    return mem_total;
+}
+
+// speed test for in-memory bit vectors
+// benchmark performs a mix of logical operations
+//
+void speed_test_bv_index(const bv_index& bvi)
+{
+    TBVector bv_join; // OR join vector
+    
+    bm::chrono_taker tt1("1. BitVector index operations", 1, &timing_map);
+
+    // join all vectors using OR operation
+    for (bv_index::map_type::const_iterator it = bvi.idx_.begin();
+         it != bvi.idx_.end();
+         ++it)
+    {
+        const TBVector* bvp = it->second;
+        
+        bv_join |= *bvp;
+    } // for
+    
+    // a group of random vectors from the index map, compute OR
+    // then compute AND with the join vector
+    //
+    TBVector bv_res(bm::BM_GAP);
+    std::vector<unsigned> result_set;
+    result_set.reserve(result_set_cnt); // memory reservation to avoid reallocs
+    
+    for (unsigned i = 0; i < benchmark_ops; ++i)
+    {
+        bv_res.clear(true); // free all blocks
+        result_set.resize(0);
+        
+        for (unsigned j = 0; j < sample_cnt; ++j)
+        {
+            unsigned id = rand() % index_size;
+            bv_index::map_type::const_iterator it = bvi.idx_.find(id);
+            if (it == bvi.idx_.end())
+                continue;
+            const TBVector& bv = *(it->second);
+            bv_res |= bv;
+        }
+        
+        bv_res &= bv_join;
+        
+        // enumerate the final result set, extract first N elements
+        //
+        TBVector::enumerator en = bv_res.first();
+        for (unsigned k = 0; en.valid() && k < result_set_cnt; ++k)
+        {
+            result_set.push_back(*en);
+        }
+
+    } // for i
+    std::cout << std::endl;
+}
+
+// speed test for in-memory serialized bit vectors
+// this function uses bm::operation_deserializer
+// to perform logical operation between a BLOB and bvector<> in memory
+// and avoids extra decompression overhead
+//
+void speed_test_bvs_index(const bvs_index& bvs)
+{
+    TBVector bv_join; // OR join vector
+    
+    BM_DECLARE_TEMP_BLOCK(tb)
+
+    bm::operation_deserializer<TBVector> des;
+    
+    bm::chrono_taker tt1("2. BitVector BLOB operations", 1, &timing_map);
+
+    // join all vectors using OR operation
+    for (bvs_index::map_type::const_iterator it = bvs.idx_.begin();
+         it != bvs.idx_.end();
+         ++it)
+    {
+        const bvs_index::buffer_type& svect = it->second;
+        if (svect.size() == 0)
+        {
+            std::cerr << "Error! Empty buffer detected." << std::endl;
+            exit(1);
+        }
+        const unsigned char* buf = it->second.data();
+        
+        des.deserialize(bv_join, buf, tb, bm::set_OR);
+    } // for
+
+    // a group of random vectors from the index map, compute OR
+    // then compute AND with the join vector
+    //
+    TBVector bv_res(bm::BM_GAP);
+    std::vector<unsigned> result_set;
+    result_set.reserve(result_set_cnt); // memory reservation to avoid reallocs
+    
+    for (unsigned i = 0; i < benchmark_ops; ++i)
+    {
+        bv_res.clear(true); // free all blocks
+        result_set.resize(0);
+        
+        for (unsigned j = 0; j < sample_cnt; ++j)
+        {
+            unsigned id = rand() % index_size;
+            bvs_index::map_type::const_iterator it = bvs.idx_.find(id);
+            if (it == bvs.idx_.end())
+                continue;
+            
+            const bvs_index::buffer_type& svect = it->second;
+            const unsigned char* buf = it->second.data();
+            des.deserialize(bv_res, buf, tb, bm::set_OR);
+        } // for j
+        
+        bv_res &= bv_join;
+        
+        // enumerate the final result set, extract first N elements
+        //
+        TBVector::enumerator en = bv_res.first();
+        for (unsigned k = 0; en.valid() && k < result_set_cnt; ++k)
+        {
+            result_set.push_back(*en);
+        }
+        //std::cout << "result=" << result_set.size() << " " << std::flush;
+
+    } // for i
+
+    std::cout << std::endl;
+}
+
+void speed_test_vect_index(const vect_index& vecti)
+{
+    TBVector bv_join; // OR join vector
+    
+    bm::chrono_taker tt1("3. vector<unsigned> operations", 1, &timing_map);
+
+    // join all vectors using OR operation
+    for (vect_index::map_type::const_iterator it = vecti.idx_.begin();
+         it != vecti.idx_.end();
+         ++it)
+    {
+        const vect_index::buffer_type& vect = it->second;
+        if (vect.size() == 0)
+        {
+            std::cerr << "Error! Empty vector detected." << std::endl;
+            exit(1);
+        }
+        
+        bm::combine_or(bv_join, vect.begin(), vect.end());
+    } // for
+
+    // a group of random vectors from the index map, compute OR
+    // then compute AND with the join vector
+    //
+    TBVector bv_res(bm::BM_GAP);
+    std::vector<unsigned> result_set;
+    result_set.reserve(result_set_cnt); // memory reservation to avoid reallocs
+    
+    for (unsigned i = 0; i < benchmark_ops; ++i)
+    {
+        bv_res.clear(true); // free all blocks
+        result_set.resize(0);
+        
+        for (unsigned j = 0; j < sample_cnt; ++j)
+        {
+            unsigned id = rand() % index_size;
+            vect_index::map_type::const_iterator it = vecti.idx_.find(id);
+            if (it == vecti.idx_.end())
+                continue;
+            
+            const vect_index::buffer_type& vect = it->second;
+            
+            bm::combine_or(bv_join, vect.begin(), vect.end());
+        } // for j
+        
+        bv_res &= bv_join;
+        
+        // enumerate the final result set, extract first N elements
+        //
+        TBVector::enumerator en = bv_res.first();
+        for (unsigned k = 0; en.valid() && k < result_set_cnt; ++k)
+        {
+            result_set.push_back(*en);
+        }
+        //std::cout << "result=" << result_set.size() << " " << std::flush;
+
+    } // for i
+
+    std::cout << std::endl;
+}
+
+
 
 
 int main(void)
 {
     try
     {
-        bv_index bvi;
+        bv_index  bvi; // regular in-memory index id to bvector<>
+        bvs_index bvs; // compressed in-memory index id to bvector<> BLOB
+        vect_index vecti; // index based on plain uncompressed vector<unsigned>
         
         generate_bv_index(bvi);
         
-        size_t mem_total = calc_memory_footprint(bvi);
+        size_t bv_mem_total = calc_memory_footprint(bvi);
+        size_t bv_mem_total_MB = bv_mem_total / (1024*1024);
         
-        std::cout << "bvector index memory footprint = " << mem_total << std::endl;
+        std::cout << "bvector index memory footprint = "
+                  << bv_mem_total << " (" << bv_mem_total_MB << "MB)"
+                  << std::endl;
         
+        size_t bvs_mem_total = convert_bv2bvs(bvi, bvs);
+        size_t bvs_mem_total_MB = bvs_mem_total / (1024*1024);
+
+        std::cout << "bvector BLOB index memory footprint = "
+                  << bvs_mem_total << " (" << bvs_mem_total_MB << "MB)"
+                  << std::endl;
+
+        size_t vecti_mem_total = convert_bv2vect(bvi, vecti);
+        size_t vecti_mem_total_MB = vecti_mem_total / (1024*1024);
+
+        std::cout << "vector<unsigned> index memory footprint = "
+                  << vecti_mem_total << " (" << vecti_mem_total_MB << "MB)"
+                  << std::endl;
+
         
-        getchar();
+
+        speed_test_bv_index(bvi);
+        speed_test_bvs_index(bvs);
+        speed_test_vect_index(vecti);
+
+        std::cout << std::endl << "Timings (ms):" << std::endl;
+        bm::chrono_taker::print_duration_map(timing_map);
+
+
+        //getchar();
 
     }
     catch(std::exception& ex)
