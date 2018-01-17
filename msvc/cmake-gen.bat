687f2e7ef90bb391f647e4f81e245a4042f25d38
akuznets <anatoliy.kuznetsov@gmail.com>
2018-01-16 19:56:11 -0500

    cmake build scripts improved to support MSVC optimizations for SSE4.2 and AVX2

--- msvc/cmake-gen.bat
+++ msvc/cmake-gen.bat
@@ -1,1 +1,8 @@
+REM Helper file to generate MSVC build environment with cmake
+
+REM Assign optimization options BMSSE42OPT or BMAVX2OPT
+REM cmake -DBMOPTFLAGS:STRING=BMSSE42OPT ..
+REM cmake -DBMOPTFLAGS:STRING=BMAVX2OPT ..
+
+REM Generate Visual Studio project files
 cmake .. -G "Visual Studio 15 2017 Win64"
