03a4398a1720e7fec2d3b94b3ab6b3ee3965dd06
akuznets <anatoliy.kuznetsov@gmail.com>
2018-02-27 19:21:05 -0500

    Helper BAT to do cmd-line build

---
+++ lang-maps/msvc/msvc-build.bat
@@ -0,0 +1,3 @@
+REM MSVC Win64 buld script
+set msdevenv="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.com"
+%msdevenv% /build Release /out build.log bmlangmap.sln
