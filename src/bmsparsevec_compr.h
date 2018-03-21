ef8b25b7eb2af2177ad157d13cd93b2a6d04d5a1
tlk00 <anatoliy.kuznetsov@gmail.com>
2018-03-21 18:46:34 -0400

    work in progress on compressed sparse vector

--- src/bmsparsevec_compr.h
+++ src/bmsparsevec_compr.h
@@ -40,11 +40,11 @@ class compressed_sparse_vector
 {
 public:
     typedef Val                                      value_type;
+    typedef const value_type&                        const_reference;
     typedef bm::id_t                                 size_type;
     typedef SV                                       sparse_vector_type;
     typedef typename SV::bvector_type                bvector_type;
     typedef bvector_type*                            bvector_type_ptr;
-    typedef const value_type&                        const_reference;
     typedef typename bvector_type::allocator_type    allocator_type;
     typedef typename bvector_type::allocation_policy allocation_policy_type;
 public:
@@ -67,6 +67,12 @@ public:
         }
         return *this;
     }
+
+    /*!
+        \brief check if another vector has the same content
+        \return true, if it is the same
+    */
+    bool equal(const compressed_sparse_vector<Val, SV>& csv) const;
     
     /*!
         \brief set specified element with bounds checking and automatic resize
@@ -78,6 +84,13 @@ public:
         \param v   - element value
     */
     void push_back(size_type idx, value_type v);
+    
+    /*!
+        \brief Load compressed vector from a sparse vector (with NULLs)
+     
+        \param sv_src - source sparse vector
+    */
+    void load_from(const sparse_vector_type& sv_src);
 
 
 protected:
@@ -124,10 +137,59 @@ void compressed_sparse_vector<Val, SV>::push_back(size_type idx, value_type v)
     
     bvector_type* bv_null = sv_.get_null_bvect();
     BM_ASSERT(bv_null);
+    
     bv_null->set(idx);
     sv_.push_back(v);
     
     max_id_ = idx;
+}
+
+//---------------------------------------------------------------------
+
+template<class Val, class SV>
+bool compressed_sparse_vector<Val, SV>::equal(const compressed_sparse_vector<Val, SV>& csv) const
+{
+    if (this == &csv)
+        return true;
+    if (max_id_ != csv.max_id_)
+        return false;
+    bool same_sv = sv_.equal(csv.sv_);
+    return same_sv;
+}
+
+//---------------------------------------------------------------------
+
+template<class Val, class SV>
+void compressed_sparse_vector<Val, SV>::load_from(const sparse_vector_type& sv_src)
+{
+    sv_.clear();
+    max_id_ = 0;
+    
+    const bvector_type* bv_null_src = sv_src.get_null_bvector();
+    if (!bv_null_src)
+    {
+        BM_ASSERT(bv_null_src);
+        return;
+    }
+    
+    bvector_type* bv_null = sv_.get_null_bvect();
+    BM_ASSERT(bv_null);
+    *bv_null = *bv_null_src;
+    
+    bm::bvector_rank_compressor<bvector_type> rank_compr; // re-used for plains
+    
+    unsigned src_plains = sv_src.plains();
+    for (unsigned i = 0; i < src_plains; ++i)
+    {
+        const bvector_type* bv_src_plain = sv_src.get_plain(i);
+        if (bv_src_plain)
+        {
+            bvector_type* bv_plain = sv_.get_plain(i);
+            rank_compr.compress(*bv_plain, *bv_null, *bv_src_plain);
+        }
+    } // for
+    
+    max_id_ = bv_null->count()-1;
 }
 
 //---------------------------------------------------------------------
