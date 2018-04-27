59b5e678fd9b80b4fdc94b3c554e249ccdffbc40
tlk00 <anatoliy.kuznetsov@gmail.com>
2018-04-26 21:44:28 -0400

    script to run tests on macos in parallel

---
+++ tests/stress/run_macos_pl.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# parallel exucion of tests in different terminal windows for MacOS
+
+open -a Terminal "./stress_debug"
+open -a Terminal "./stress_release"
+open -a Terminal "./stress_release_sse2"
+open -a Terminal "./stress_release_sse42"
+open -a Terminal "./stress_release_avx2"
+
