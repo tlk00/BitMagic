1c476cba12c361e237d1716706f203330ddaaa32
tlk00 <anatoliy.kuznetsov@gmail.com>
2018-07-18 18:29:11 -0400

    version increment

--- src/bmconst.h
+++ src/bmconst.h
@@ -177,8 +177,8 @@ template<bool T> struct _copyright
 };
 
 template<bool T> const char _copyright<T>::_p[] = 
-    "BitMagic C++ Library. v.3.12.5 (c) 2002-2018 Anatoliy Kuznetsov.";
-template<bool T> const unsigned _copyright<T>::_v[3] = {3, 12, 5};
+    "BitMagic C++ Library. v.3.12.6 (c) 2002-2018 Anatoliy Kuznetsov.";
+template<bool T> const unsigned _copyright<T>::_v[3] = {3, 12, 6};
 
 
 template<bool T> struct DeBruijn_bit_position
