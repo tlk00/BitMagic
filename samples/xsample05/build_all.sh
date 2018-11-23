51b33c0f63c4db6c130502106605c9a663513f91
akuznets <anatoliy.kuznetsov@gmail.com>
2018-11-23 13:06:18 -0500

    Add build script

---
+++ samples/xsample05/build_all.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Script for in-place build for regular, SSE4.2 and AVX2
+#
+
+cd ../../
+. ./bmenv.sh
+cd -
+
+rm  ./xsample05* ./xsample05
+make clean
+
+make BMOPTFLAGS=-DBMSSE42OPT rebuild
+mv ./xsample05 ./xsample05_sse42
+
+make BMOPTFLAGS=-DBMAVX2OPT rebuild
+mv ./xsample05 ./xsample05_avx2
+
+make rebuild
+cp ./xsample05 ./xsample05_release
+
