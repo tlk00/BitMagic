9910bbf0edcced2a31f3763a7c02fc31fdc23720
tlk00 <anatoliy.kuznetsov@gmail.com>
2018-11-29 08:09:19 -0500

    cosmetics

--- xcode/cmake-gen.sh
+++ xcode/cmake-gen.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 # Generate out of the source xcode project using cmake
-cmake -DBMOPTFLAGS:STRING=BMSSE42OPT ..
+#cmake -DBMOPTFLAGS:STRING=BMSSE2OPT ..
+#cmake -DBMOPTFLAGS:STRING=BMSSE42OPT ..
 #cmake -DBMOPTFLAGS:STRING=BMAVX2OPT ..
-#cmake -DBMOPTFLAGS:STRING=none ..
+cmake -DBMOPTFLAGS:STRING=none ..
 cmake .. -GXcode
