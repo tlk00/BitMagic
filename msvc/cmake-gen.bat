65db98a7a49f2886759b83893624be73769b2d49
akuznets <anatoliy.kuznetsov@gmail.com>
2018-12-24 10:42:56 -0500

    cmake build tweaks

--- msvc/cmake-gen.bat
+++ msvc/cmake-gen.bat
@@ -1,7 +1,7 @@
 REM Helper file to generate MSVC build environment with cmake
 
 REM Assign optimization options BMSSE42OPT or BMAVX2OPT
-cmake -DBMOPTFLAGS:STRING=none ..
+REM cmake -DBMOPTFLAGS:STRING=none ..
 REM cmake -DBMOPTFLAGS:STRING=BMSSE42OPT ..
 REM cmake -DBMOPTFLAGS:STRING=BMAVX2OPT ..
 
