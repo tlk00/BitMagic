9fa2bb5693743d4a2fa20efed061be06c637ec47
tlk00 <anatoliy.kuznetsov@gmail.com>
2019-06-03 19:25:13 -0400

    fixed 64-bit compat issue

--- src/bm.h
+++ src/bm.h
@@ -6582,7 +6582,7 @@ void bvector<Alloc>::copy_range_no_check(const bvector<Alloc>& bvect,
     //
     if (left)
     {
-        bm::id_t from =
+        size_type from =
             (left + bm::gap_max_bits >= left) ? 0u : left - bm::gap_max_bits;
         clear_range_no_check(from, left-1); // TODO: optimize clear from
     }
