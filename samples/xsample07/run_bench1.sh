1a6de4cb0dce40db94ed7f8d0f237931086c4eba
tlk00 <anatoliy.kuznetsov@gmail.com>
2020-04-08 12:18:56 -0400

    improved benchmark script

--- samples/xsample07/run_bench1.sh
+++ samples/xsample07/run_bench1.sh
@@ -6,11 +6,13 @@
 THREADS=4
 K_MIN=7
 K_MAX=8
-SIMD=_sse42
 
+SIMD=
+SIMD=_sse42
+#SIMD=_avx2
 for ((K = $K_MIN ; K <= K_MAX ; K++));
 do
   echo "----------------------------------------------------------------- $K"
-  ./xsample07${SIMD} -kd test_${K}.kd -kdc test_${K}.kdc -fa NC_000001.11.fa -k $K  -t -j $THREADS | tee -a run_${K}.log
+  ./xsample07${SIMD} -kd test_${K}.kd -kdc test_${K}.kdc -fa NC_000001.11.fa -k $K  -t -j $THREADS | tee -a run_${K}${SIMD}.log
 done
 
