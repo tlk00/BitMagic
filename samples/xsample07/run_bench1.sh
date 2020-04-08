9ae82512c15592388ffde0182788fdff63408ff8
tlk00 <anatoliy.kuznetsov@gmail.com>
2020-04-08 13:53:17 -0400

    cosmetic improvements in benchmarking scripts

--- samples/xsample07/run_bench1.sh
+++ samples/xsample07/run_bench1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Script for sample benchmarking
 #
--- samples/xsample07/run_bench2.sh
+++ samples/xsample07/run_bench2.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Script for sample benchmarking
 #
@@ -15,7 +15,7 @@ K=9
 
 for ((THREADS = $J_MIN ; THREADS <= J_MAX ; THREADS+=2));
 do
-  echo "----------------------------------------------------------------- $K"
+  echo "----------------------------------------------------------- ${THREADS}"
   ./xsample07${SIMD} -kd test_${K}.kd -kdc test_${K}.kdc -fa NC_000001.11.fa -k $K  -t -j $THREADS | tee -a run_${K}${SIMD}_p${THREADS}.log
 done
 
