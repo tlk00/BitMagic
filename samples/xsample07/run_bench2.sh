6fec3cc42a8a518161e543bc56068efd1a7a49e4
tlk00 <anatoliy.kuznetsov@gmail.com>
2020-04-13 14:18:00 -0400

    benchmark tweaks

--- samples/xsample07/run_bench2.sh
+++ samples/xsample07/run_bench2.sh
@@ -4,16 +4,16 @@
 #
 
 
-J_MIN=2
-J_MAX=4
+J_MIN=1
+J_MAX=8
 
-SIMD=
-SIMD=_sse42
-#SIMD=_avx2
+#SIMD=
+#SIMD=_sse42
+SIMD=_avx2
 
-K=9
+K=16
 
-for ((THREADS = $J_MIN ; THREADS <= J_MAX ; THREADS+=2));
+for ((THREADS = $J_MIN ; THREADS <= J_MAX ; THREADS+=1));
 do
   echo "----------------------------------------------------------- ${THREADS}"
   ./xsample07${SIMD} -kd test_${K}.kd -kdc test_${K}.kdc -fa NC_000001.11.fa -k $K  -t -j $THREADS | tee -a run_${K}${SIMD}_p${THREADS}.log
