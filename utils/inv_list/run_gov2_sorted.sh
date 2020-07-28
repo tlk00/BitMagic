6cc563a50d8b180b927654c7c7d118a87d3992e2
tlk00 <anatoliy.kuznetsov@gmail.com>
2020-07-28 17:15:56 -0400

    improved benchmarking sample scripts

--- utils/inv_list/run_gov2_sorted.sh
+++ utils/inv_list/run_gov2_sorted.sh
@@ -4,80 +4,84 @@ echo
 echo "BitMagic inverted collections experiments"
 echo
 
+#IN_PATH="./"
+OUT_PATH="./"
+IN_PATH=/Volumes/WD-MacOS/int_set_bench/Gov2Flat/
+
 echo "Compressing Sorted Gov2 set (regular build). BIC-CM (6)"
-./bminv_reg -t -s -level 6 -u32in gov2.sorted -bvout gov2.sorted.bv.cm6
+./bminv_reg -t -s -level 6 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm6
 
 echo
 echo "Verify Sorted Gov2 set (regular build). BIC-CM (6)"
-./bminv_reg -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.cm6
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm6
 
 echo
 echo "Compressing Sorted Gov2 set (SSE42). BIC-CM (6)"
-./bminv_sse42 -t -s -level 6 -u32in gov2.sorted -bvout gov2.sorted.bv.cm6
+./bminv_sse42 -t -s -level 6 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm6
 
 echo
 echo "Verify Sorted Gov2 set (SSE42). BIC-CM (6)"
-./bminv_sse42 -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.cm6
+./bminv_sse42 -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm6
 
 echo
 echo "Compressing Sorted Gov2 set (AVX2). BIC-CM (6)"
-./bminv_avx2 -t -s -level 6 -u32in gov2.sorted -bvout gov2.sorted.bv.cm6
+./bminv_avx2 -t -s -level 6 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm6
 
 echo
 echo "Verify Sorted Gov2 set (AVX2). BIC-CM (6)"
-./bminv_avx2 -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.cm6
+./bminv_avx2 -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm6
 
 
 
 echo "Compressing Sorted Gov2 set (regular build). BIC-CM (5)"
-./bminv_reg -t -s -level 5 -u32in gov2.sorted -bvout gov2.sorted.bv.cm5
+./bminv_reg -t -s -level 5 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm5
 
 echo
 echo "Verify Sorted Gov2 set (regular build). BIC-CM (5)"
-./bminv_reg -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.cm5
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm5
 
 echo
 echo "Compressing Sorted Gov2 set (SSE42). BIC-CM (5)"
-./bminv_sse42 -t -s -level 5 -u32in gov2.sorted -bvout gov2.sorted.bv.cm5
+./bminv_sse42 -t -s -level 5 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm5
 
 echo
 echo "Verify Sorted Gov2 set (SSE42). BIC-CM (5)"
-./bminv_sse42 -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.cm5
+./bminv_sse42 -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm5
 
 echo
 echo "Compressing Sorted Gov2 set (AVX2). BIC-CM (5)"
-./bminv_avx2 -t -s -level 5 -u32in gov2.sorted -bvout gov2.sorted.bv.cm5
+./bminv_avx2 -t -s -level 5 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm5
 
 echo
 echo "Verify Sorted Gov2 set (AVX2). BIC-CM (5)"
-./bminv_avx2 -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.cm5
+./bminv_avx2 -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm5
 
 
 
 echo "Compressing Sorted Gov2 set (regular build). Elias Gamma."
-./bminv_reg -t -s -level 4 -u32in gov2.sorted -bvout gov2.sorted.bv.eg
+./bminv_reg -t -s -level 4 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.eg
 
 echo
 echo "Verify Sorted Gov2 set (regular build). Elias Gamma."
-./bminv_reg -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.eg
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.eg
 
 echo
 echo "Compressing Sorted Gov2 set (SSE42). Elias Gamma."
-./bminv_sse42 -t -s -level 4 -u32in gov2.sorted -bvout gov2.sorted.bv.eg
+./bminv_sse42 -t -s -level 4 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.eg
 
 echo
 echo "Verify Sorted Gov2 set (SSE42). Elias Gamma."
-./bminv_sse42 -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.eg
+./bminv_sse42 -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.eg
 
 echo
 echo "Compressing Sorted Gov2 set (AVX2). Elias Gamma."
-./bminv_avx2 -t -s -level 4 -u32in gov2.sorted -bvout gov2.sorted.bv.eg
+./bminv_avx2 -t -s -level 4 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.eg
 
 echo
 echo "Verify Sorted Gov2 set (AVX2). Elias Gamma."
-./bminv_avx2 -t -s -verify -u32in gov2.sorted -bvin gov2.sorted.bv.cm
+./bminv_avx2 -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.eg
 
 echo
-ls -lap gov2.*
+ls -lap ${OUT_PATH}gov2.*
 echo
 
--- utils/inv_list/run_gov2_unsorted.sh
+++ utils/inv_list/run_gov2_unsorted.sh
@@ -4,79 +4,83 @@ echo
 echo "BitMagic inverted collections experiments"
 echo
 
+OUT_PATH="./"
+#IN_PATH="./"
+IN_PATH=/Volumes/WD-MacOS/int_set_bench/Gov2Flat/
+
 echo "Compressing Sorted Gov2 set (regular build). BIC-CM (6)"
-./bminv_reg -t -s -level 6 -u32in gov2.unsorted -bvout gov2.unsorted.bv.cm6
+./bminv_reg -t -s -level 6 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.cm6
 
 echo
 echo "Verify Sorted Gov2 set (regular build). BIC-CM (6)"
-./bminv_reg -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.cm6
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.cm6
 
 echo
 echo "Compressing Sorted Gov2 set (SSE42). BIC-CM (6)"
-./bminv_sse42 -t -s -level 6 -u32in gov2.unsorted -bvout gov2.unsorted.bv.cm6
+./bminv_sse42 -t -s -level 6 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.cm6
 
 echo
 echo "Verify Sorted Gov2 set (SSE42). BIC-CM (6)"
-./bminv_sse42 -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.cm6
+./bminv_sse42 -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.cm6
 
 echo
 echo "Compressing Sorted Gov2 set (AVX2). BIC-CM (6)"
-./bminv_avx2 -t -s -level 6 -u32in gov2.unsorted -bvout gov2.unsorted.bv.cm6
+./bminv_avx2 -t -s -level 6 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.cm6
 
 echo
 echo "Verify Sorted Gov2 set (AVX2). BIC-CM (6)"
-./bminv_avx2 -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.cm6
+./bminv_avx2 -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.cm6
 
 
 
 
 echo "Compressing Sorted Gov2 set (regular build). BIC-CM (5)"
-./bminv_reg -t -s -level 5 -u32in gov2.unsorted -bvout gov2.unsorted.bv.cm5
+./bminv_reg -t -s -level 5 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.cm5
 
 echo
 echo "Verify Sorted Gov2 set (regular build). BIC-CM (5)"
-./bminv_reg -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.cm5
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.cm5
 
 echo
 echo "Compressing Sorted Gov2 set (SSE42). BIC-CM (5)"
-./bminv_sse42 -t -s -level 5 -u32in gov2.unsorted -bvout gov2.unsorted.bv.cm5
+./bminv_sse42 -t -s -level 5 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.cm5
 
 echo
 echo "Verify Sorted Gov2 set (SSE42). BIC-CM (5)"
-./bminv_sse42 -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.cm5
+./bminv_sse42 -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.cm5
 
 echo
 echo "Compressing Sorted Gov2 set (AVX2). BIC-CM (5)"
-./bminv_avx2 -t -s -level 5 -u32in gov2.unsorted -bvout gov2.unsorted.bv.cm5
+./bminv_avx2 -t -s -level 5 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.cm5
 
 echo
 echo "Verify Sorted Gov2 set (AVX2). BIC-CM (5)"
-./bminv_avx2 -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.cm5
+./bminv_avx2 -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.cm5
 
 
 
 echo "Compressing Sorted Gov2 set (regular build). Elias Gamma."
-./bminv_reg -t -s -level 4 -u32in gov2.unsorted -bvout gov2.unsorted.bv.eg
+./bminv_reg -t -s -level 4 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.eg
 
 echo
 echo "Verify Sorted Gov2 set (regular build). Elias Gamma."
-./bminv_reg -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.eg
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.eg
 
 echo
 echo "Compressing Sorted Gov2 set (SSE42). Elias Gamma."
-./bminv_sse42 -t -s -level 4 -u32in gov2.unsorted -bvout gov2.unsorted.bv.eg
+./bminv_sse42 -t -s -level 4 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.eg
 
 echo
 echo "Verify Sorted Gov2 set (SSE42). Elias Gamma."
-./bminv_sse42 -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.eg
+./bminv_sse42 -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.eg
 
 echo
 echo "Compressing Sorted Gov2 set (AVX2). Elias Gamma."
-./bminv_avx2 -t -s -level 4 -u32in gov2.unsorted -bvout gov2.unsorted.bv.eg
+./bminv_avx2 -t -s -level 4 -u32in ${IN_PATH}gov2.unsorted -bvout ${OUT_PATH}gov2.unsorted.bv.eg
 
 echo
 echo "Verify Sorted Gov2 set (AVX2). Elias Gamma."
-./bminv_avx2 -t -s -verify -u32in gov2.unsorted -bvin gov2.unsorted.bv.cm
+./bminv_avx2 -t -s -verify -u32in ${IN_PATH}gov2.unsorted -bvin ${OUT_PATH}gov2.unsorted.bv.eg
 
 echo
 ls -lap gov2.*
