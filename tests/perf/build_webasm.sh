155a15d0a597d64974ae54ed2ee55e550ff14433
tlk00 <anatoliy.kuznetsov@gmail.com>
2021-07-05 12:18:41 -0400

    Use -std=cxx-17 in WASM build scripts

--- tests/perf/build_webasm.sh
+++ tests/perf/build_webasm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-emcc -std=c++11 -s ALLOW_MEMORY_GROWTH=1 -O2 -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0 -fno-rtti -I./../../src perf.cpp -o perf.html
+emcc -std=c++17 -s ALLOW_MEMORY_GROWTH=1 -O2 -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0 -fno-rtti -I./../../src perf.cpp -o perf.html
 #emcc -std=c++11 -s ALLOW_MEMORY_GROWTH=1 -O2 -s WASM=1 -s -fno-rtti -I./../../src perf.cpp -o perf.html
--- tests/perf/build_webasm_simd.sh
+++ tests/perf/build_webasm_simd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-emcc -std=c++11 -s ALLOW_MEMORY_GROWTH=1 -O2 -msse4.2 -msimd128 -D BMWASMSIMDOPT -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0  -fno-rtti -I./../../src perf.cpp -o perf.html
+emcc -std=c++17 -s ALLOW_MEMORY_GROWTH=1 -O2 -msse4.2 -msimd128 -D BMWASMSIMDOPT -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0  -fno-rtti -I./../../src perf.cpp -o perf.html
 
 #emcc -std=c++11 -g3 --closure 0 --profiling --emit-symbol-map -s ALLOW_MEMORY_GROWTH=1 -O2 -msse4.2 -msimd128 -D BMWASMSIMDOPT -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0  -fno-rtti -I./../../src perf.cpp -o perf.html
 #emcc -std=c++11 -s ALLOW_MEMORY_GROWTH=1 -O2 -s WASM=1 -s -fno-rtti -I./../../src perf.cpp -o perf.html
