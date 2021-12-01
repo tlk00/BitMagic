8aa9a90abc4a1433363853400a055762882afc7e
tlk00 <anatoliy.kuznetsov@gmail.com>
2021-12-01 18:38:52 -0500

    fixed minor scripting issues

--- tests/stress/run_all_parallel.sh
+++ tests/stress/run_all_parallel.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #  run_all_parallel.sh
 #  
--- tests/stress/run_all_tests.sh
+++ tests/stress/run_all_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #  run_all_tests.sh
 #  
@@ -6,6 +6,7 @@
 #
 
 ./run_all_parallel.sh || exit 1
+./run_all_parallel.sh gcc || exit 1
 ./run_all_parallel.sh sse2 || exit 1
 ./run_all_parallel.sh sse42 || exit 1
 ./run_all_parallel.sh avx2 || exit 1
