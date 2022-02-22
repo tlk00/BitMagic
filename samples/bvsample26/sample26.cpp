4e2beba26e1935296b8b13bc024696502322d8e4
tlk00 <anatoliy.kuznetsov@gmail.com>
2022-02-22 09:18:00 -0500

    fixed minor compilation issue

--- samples/bvsample26/sample26.cpp
+++ samples/bvsample26/sample26.cpp
@@ -33,6 +33,7 @@ For more information please visit:  http://bitmagic.io
 */
 #include <stdlib.h>
 #include <iostream>
+#include <cassert>
 
 #include "bm.h"
 #include "bmundef.h" /* clear the pre-proc defines from BM */
