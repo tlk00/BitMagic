2ae9b5ae84dad382d5e73f4bfb0287563e48ac73
Anatoliy Kuznetsov <anatoliy.kuznetsov@gmail.com>
2024-06-02 12:01:18 -0400

    ARM based utility script to run experiments

---
+++ utils/inv_list/run_gov2_sorted_arm.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo
+echo "BitMagic inverted collections experiments"
+echo
+
+#IN_PATH="./"
+OUT_PATH="./"
+IN_PATH=/Volumes/WD-MacOS/int_set_bench/Gov2Flat/
+
+echo "Compressing Sorted Gov2 set (regular build). BIC-CM (6)"
+./bminv_reg -t -s -level 6 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm6
+
+echo
+echo "Verify Sorted Gov2 set (regular build). BIC-CM (6)"
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm6
+
+
+
+echo "Compressing Sorted Gov2 set (regular build). BIC-CM (5)"
+./bminv_reg -t -s -level 5 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.cm5
+
+echo
+echo "Verify Sorted Gov2 set (regular build). BIC-CM (5)"
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.cm5
+
+
+echo "Compressing Sorted Gov2 set (regular build). Elias Gamma."
+./bminv_reg -t -s -level 4 -u32in ${IN_PATH}gov2.sorted -bvout ${OUT_PATH}gov2.sorted.bv.eg
+
+echo
+echo "Verify Sorted Gov2 set (regular build). Elias Gamma."
+./bminv_reg -t -s -verify -u32in ${IN_PATH}gov2.sorted -bvin ${OUT_PATH}gov2.sorted.bv.eg
+
+
+echo
+ls -lap ${OUT_PATH}gov2.*
+echo
+
